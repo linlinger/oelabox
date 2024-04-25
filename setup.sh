3f341705368227f9a6a4cd5a344886914036e156
林凌儿 <32541118+linlinger@users.noreply.github.com>
2024-04-25 16:58:47 +0800

    Add initial support for Arch Linux Based distros
    Tested on EndeavourOS

--- setup.sh
+++ setup.sh
@@ -12,6 +12,8 @@ function check_installed() {
         rpm -q "$1" &> /dev/null
     elif [ -n "$(command -v dpkg-query)" ]; then
         dpkg-query -W -f='${Status}' "$1" 2>/dev/null | grep -c "ok installed"
+    elif [ -n "$(command -v pacman)" ]; then  # Corrected condition for pacman
+        pacman -Q "${1}" &> /dev/null
     else
         echo_info "Unsupported package manager."
         exit 1
@@ -35,6 +37,10 @@ if ! check_installed ansible; then
         apt update
         apt install python3-pip
         pip3 install ansible
+    elif [ -n "$(command -v pacman)" ]; then
+        pacman -S ansible --noconfirm
+        echo "Installing Ansible community collections"
+        ansible-galaxy collection install community.general
     else
         echo_info "Unsupported package manager."
         exit 1
